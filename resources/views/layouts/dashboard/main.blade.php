e0132f262f6109d40474a45e7d43c9a6db212cbc
Fahmy4you <fahmi4you2208@gmail.com>
2025-03-12 11:51:25 +0700

    Change Databases

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     @vite('resources/css/app.css')
-    <title>MARBLE BLUE | {{ $title ??  config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'HOTEL BLUE') }} | {{ $title ??  config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" href="{{ asset('image/landing_page/favicon.ico') }}" type="image/x-icon">
 </head>
 <body class="text-gray-800 font-inter">
     
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
-    <title>MARBLE BLUE | {{ $title ??  config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'HOTEL BLUE') }} | {{ $title ??  config('app.name', 'Laravel') }}</title>
     <!-- favicon link -->
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('image/landing_page/favicon.ico') }}" type="image/x-icon">
     <!-- google font link -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <!-- tailwind css cdn -->
