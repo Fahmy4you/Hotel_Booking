9db68bc2c6e0669c8c615e3c26bc8b4340298a30
Fahmy4you <fahmi4you2208@gmail.com>
2025-03-12 12:25:14 +0700

    Add Sticky

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -29,7 +29,7 @@
 
 <!-- ====== header ====== -->
 
-<header x-data="{navbarOpen: false}" class="absolute sticky left-0 top-0 z-50 bg-white/90 w-full backdrop-blur">
+<header x-data="{navbarOpen: false}" class="sticky left-0 top-0 z-50 bg-white/90 w-full backdrop-blur">
     <div class="mx-auto h-[90px] max-w-7xl px-8 md:px-6">
         <div class="relative flex h-full items-center justify-between border-b border-slate-500/10">
             <!-- logo -->
